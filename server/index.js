dafb0f11a5a62dfd1db8e41ce6de25eba225e691
Kenneth Nguyen <n.kennethcs@gmail.com>
2021-05-02 23:46:08 -0700

    configure port and node engine variable for heroku

--- server/index.js
+++ server/index.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
   return res.send("Hello World!");
 });
 
-app.listen(8000, () => console.log("Listening on Port 8000"));
+app.listen(process.env.PORT || 8000, () => console.log("Listening on Port 8000"));
