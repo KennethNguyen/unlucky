0a981e6ecc250f778178df5d1abc4b384fec8192
Kenneth Nguyen <n.kennethcs@gmail.com>
2021-05-10 22:17:59 -0700

    update Post component color and comment style

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -18,7 +18,15 @@ import { FaThumbsUp } from "react-icons/fa";
 
 const Post = () => {
   return (
-    <Box w="90%" borderWidth={2} mx="auto" boxShadow="lg" rounded="md">
+    <Box
+      bg="#011627"
+      w="90%"
+      p={2}
+      borderWidth={2}
+      mx="auto"
+      boxShadow="lg"
+      rounded="md"
+    >
       {/* Heading */}
       <Flex justify="center" align="center" p={2}>
         <Avatar name="Sasuke Uchiha" />
@@ -29,19 +37,21 @@ const Post = () => {
         <IconButton aria-label="Edit post" icon={<EditIcon />} />
       </Flex>
       <Divider />
+
       {/* Content */}
       <VStack p={2} align="flex-start">
         <Text fontWeight="bold" letterSpacing="0.5px">
           Title
         </Text>
-        <Text textAlign="start">
+        <Text opacity="0.9" textAlign="start">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </Text>
         <Text fontWeight="bold">#Hashtag</Text>
       </VStack>
+
       {/* Post action buttons */}
       <HStack p={2}>
         <Button
@@ -64,40 +74,50 @@ const Post = () => {
         </Button>
       </HStack>
       <Divider />
+
       {/* Post comments */}
-      <Flex justify="center" align="flex-start" p={2}>
-        <Avatar size="sm" name="Sasuke Uchiha" />
-        <VStack ml={2} align="flex-start">
-          <Text fontSize="sm">Username</Text>
-          <Text fontSize="sm" opacity="0.6">
-            3 hours ago
-          </Text>
-        </VStack>
-        <Text flex="1" ml={4} textAlign="left" fontSize="md">
-          This is the text of the comment. This is the text of the comment This
-          is the text of the comment.
-        </Text>
-        <HStack p={2}>
-          <Button
-            leftIcon={<FaThumbsUp />}
-            bg="blue.600"
-            _hover={{ bg: "blue.500" }}
-            variant="solid"
-            size="sm"
-          >
-            Like
-          </Button>
-          <IconButton
-            aria-label="Delete comment"
-            icon={<DeleteIcon />}
-            bg="red.600"
-            _hover={{ bg: "red.500" }}
-            variant="solid"
-            size="sm"
-          />
-        </HStack>
+      <Flex direction="column" justify="center" align="flex-start" p={2} ml={6}>
+        <Box>
+          <Flex>
+            <Avatar size="sm" name="Sasuke Uchiha" />
+            <HStack ml={2} align="center">
+              <Text fontSize="sm">Username</Text>
+              <Text fontSize="sm" opacity="0.6">
+                3 hours ago
+              </Text>
+            </HStack>
+          </Flex>
+          <Flex>
+            <Text ml={1} mt={1} textAlign="left" opacity="0.9" fontSize="md">
+              This is the text of the comment. This is the text of the comment.
+              This is the text of the comment.
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <HStack p={2}>
+            <Button
+              leftIcon={<FaThumbsUp />}
+              bg="blue.600"
+              _hover={{ bg: "blue.500" }}
+              variant="solid"
+              size="sm"
+            >
+              Like
+            </Button>
+            <IconButton
+              aria-label="Delete comment"
+              icon={<DeleteIcon />}
+              bg="red.600"
+              _hover={{ bg: "red.500" }}
+              variant="solid"
+              size="sm"
+            />
+          </HStack>
+        </Box>
       </Flex>
       <Divider />
+
       {/* Comment input */}
       <InputGroup>
         <Input
